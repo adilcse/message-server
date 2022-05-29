377b54026628383ec9b05040dad86d48e6eedbf2
Sahil <zarvis.is.homepc@gmail.com>
2022-05-29 16:52:10 +0530

    update cookie

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
-  cookie: { maxAge: oneMonth },
+  cookie: { maxAge: oneMonth , secure: true},
 }));
 app.use(cors({
     origin: ['http://localhost:3000'],
